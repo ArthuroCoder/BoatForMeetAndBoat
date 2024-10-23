0f4d1438f742e3c26b8a5815a3180f6ba4d9ab2b
Arthuro <dgiovanni52000@yahoo.fr>
2024-10-23 12:15:08 +0200

    save

---
+++ Script/BasicBoatEditorMono.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace gio
+{
+    [CustomEditor(typeof(BasicBoatMono))]
+    public class BasicBoatEditorMono : Editor
+    {
+       
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            BasicBoatMono t = (BasicBoatMono)target;
+            if (GUILayout.Button("Teleport Forward"))
+            {
+                t.TeleportForward();
+            }
+        }
+    }
+}
